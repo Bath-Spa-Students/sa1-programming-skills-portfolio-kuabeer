58afc680eb7b82274c5702bcfc5d641378c0bce0
kuabeer <abeerfatima1203@gmail.com>
2025-03-22 22:55:46 +0400

    Simple Search

---
+++ Exercise8.py
@@ -0,0 +1,20 @@
+# name list
+username = ["Jake","Zac","Ian","Ron","Sam","Dave"]
+
+# to find if Sam is in name list 
+if "Sam" in username:
+   print("We found your name in list")
+
+else:
+  print("Sorry, your name was not in list")
+
+
+# Optional Requirement
+# input to find name 
+search = input ("Enter your name: ")
+
+if search in username:
+   print("We found your name in list")
+   
+else:
+  print("sorry, your name was not in list")
