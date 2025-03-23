5ebe2ff3a149572555fcaad3a5eb8faab88a2a77
kuabeer <abeerfatima1203@gmail.com>
2025-03-23 21:53:58 +0400

    Some Counting

---
+++ Exercise7.py
@@ -0,0 +1,59 @@
+#up from 0 to 50 in increments of 1
+#for loop
+for x in range(51):
+      print(x)
+      
+#while loop 
+i = 0 
+while i <= 50:
+    print(i)
+    i+=1
+
+
+#down from 50 to 0 in decrements of 1
+#for loop
+for x in range(50,-1,-1):
+      print(x)
+
+#while loop
+i = 50
+while i >= 0:
+    print(i)
+    i-=1
+
+
+#up from 30 to 50 in increments of 1
+#for loop
+for x in range(30,51,1):
+      print(x)
+
+#while loop
+i = 30
+while i <= 50:
+    print(i)
+    i+=1
+
+
+#down from 50 to 10 in decrements of 2
+#for loop
+for x in range(50,9,-2):
+      print(x)
+
+#while loop
+i = 50
+while i >= 10:
+    print(i)
+    i-=2
+
+
+#up from 100 to 200 in increments of 5
+#for loop
+for x in range(100,201,5):
+      print(x)
+
+#while loop
+i = 100
+while i <= 200:
+    print(i)
+    i+=5
+
