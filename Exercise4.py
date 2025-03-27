c5ef3f3312a79e1819784c4b4db3a811947dddf6
kuabeer <abeerfatima1203@gmail.com>
2025-03-27 20:21:13 +0400

    Primitive Quiz

---
+++ Exercise4.py
@@ -0,0 +1,10 @@
+#ask user question 
+user = input("What is the capital of France?")
+
+# to check if answer is correct or not
+# if answer is correct
+if user == "Paris":
+  print("Yes, that's correct answer")
+# if answer is wrong 
+else:
+  print("Sorry, that's wrong answer")
