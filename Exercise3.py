21cebb00f96ad4f84f98d23a10f31c1936bf189e
kuabeer <abeerfatima1203@gmail.com>
2025-04-07 20:25:35 +0400

    Biography

---
+++ Exercise3.py
@@ -0,0 +1,10 @@
+# Store the information in a dictionary
+information= {
+    "Name": "Abeer Fatima",
+    "Hometown": "Sharjah",
+    "Age": "21 years old"
+}
+# print the values on separate lines
+print("""Name: """ + information["Name"] + """
+Hometown: """ + information["Hometown"] + """
+Age: """ + str(information["Age"]))
